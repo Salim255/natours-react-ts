f0444ce6c283e42a2583a0933aa732750576634f
Salim <doshka23@gmail.com>
2026-02-11 22:30:52 +0100

    built: app footer

--- src/main-layout/components/footer/Footer.tsx
+++ src/main-layout/components/footer/Footer.tsx
@@ -2,11 +2,20 @@ import './_footer.scss';
 
 const Footer = () => {
     return (
-        <>
-            <footer className='footer'>
-                <h1>Hello from footer</h1>
-            </footer>
-        </>
+        <footer className='footer'>
+            <div className='footer__logo'>
+                <img src='/img/logo-green.png' alt='Natour  Salim logo'/>
+            </div>
+            
+            <ul className='footer__nav'>
+                <li> <a href='#'>About us</a></li>
+                <li> <a href='#'>Download apps</a></li>
+                <li> <a href='#'>Become a guide</a></li>
+                <li> <a href='#'> Careers</a></li>
+                <li> <a href='#'> Contact </a></li>
+            </ul>    
+            <p className='footer__copyright'> &copy; Designed by Jonas Schmedtmann, built by Salim Hassan. All rights reserved. </p>
+        </footer>
     )
 }
 

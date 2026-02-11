eaef79da58e2491288fd6da85f7a9ece3ab56636
Salim <doshka23@gmail.com>
2026-02-11 23:15:15 +0100

    built: overview template

--- src/features/overview/Overview.tsx
+++ src/features/overview/Overview.tsx
@@ -1,12 +1,103 @@
 import './_overview.scss';
 
 const Overview = () => {
+    const tours: {
+        imageCover: string;
+        name: string;
+        duration: string;
+        difficulty: string;
+        summary: string;
+        locations: {length: number};
+        startLocation: {description: string};
+        startDates: Date[];
+        maxGroupSize: number;
+        price: number;
+        ratingsAverage: number;
+        ratingsQuantity: number;
+        slug: string;
+    }[] = [];
     return (
-        <>
-            <section className='overview'>
-                <h1>Hello from over view</h1>
-            </section>
-        </>
+        <div>
+            <main className='main card-container'>
+                {
+                    tours.map(tour => {
+                        return(
+                            <div className='card'>
+                                <div className='card__header'>
+                                    <div className='card__picture'>
+                                        <div className='card__picture-overlay'></div>&nbsp;
+                                        <img 
+                                            src={'/img/tours/' + `${tour.imageCover}`}
+                                            alt={tour.name}
+                                            className='card__picture-img'
+                                        />
+                                        <h3 className='heading-tertirary'>
+                                            <span>{tour.name}</span>
+                                        </h3>
+                                    </div>
+                                </div>
+                                <div className='card__details'>
+                                    <h4 className='card__sub-heading'>
+                                        {tour.difficulty} {`${tour.duration}` + '-day tour'}
+                                    </h4>
+                                    <p className='card__text'> {tour.summary}</p>
+                                       <div className="card__data">
+                                            <svg className="card__icon">
+                                            <use xlinkHref="/img/icons.svg#icon-map-pin" />
+                                            </svg>
+                                            <span>{tour.startLocation.description}</span>
+                                        </div>
+
+                                        <div className="card__data">
+                                            <svg className="card__icon">
+                                            <use xlinkHref="/img/icons.svg#icon-calendar" />
+                                            </svg>
+                                            <span>
+                                            {new Date(tour.startDates[0]).toLocaleString("fr-FR", {
+                                                month: "long",
+                                                year: "numeric",
+                                            })}
+                                            </span>
+                                        </div>
+
+                                        <div className="card__data">
+                                            <svg className="card__icon">
+                                            <use xlinkHref="/img/icons.svg#icon-flag" />
+                                            </svg>
+                                            <span>{`${tour.locations.length} stops`}</span>
+                                        </div>
+
+                                        <div className="card__data">
+                                            <svg className="card__icon">
+                                            <use xlinkHref="/img/icons.svg#icon-user" />
+                                            </svg>
+                                            <span>{`${tour.maxGroupSize} people`}</span>
+                                        </div>
+                                    </div>
+                                     {/* FOOTER */}
+                                    <div className="card__footer">
+                                    <p>
+                                        <span className="card__footer-value">{`$${tour.price}`}</span>{" "}
+                                        <span className="card__footer-text">per person</span>
+                                    </p>
+
+                                    <p className="card__ratings">
+                                        <span className="card__footer-value">{tour.ratingsAverage}</span>{" "}
+                                        <span className="card__footer-text">
+                                        {`rating (${tour.ratingsQuantity})`}
+                                        </span>
+                                    </p>
+
+                                    <a className="btn btn--green btn--small" href={`/tour/${tour.slug}`} >
+                                        Details
+                                    </a>
+                                    </div>
+                            </div>
+                        )
+                    })
+                }
+            </main>
+        </div>
     )
 }
 
--- src/main-layout/components/header/Header.tsx
+++ src/main-layout/components/header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className='header__logo'>
                 <img  alt='Natours logo' src='/img/logo-white.png' />
             </div>
-            <nav className='nav nav--users'  >
+            <nav className='nav nav--user'  >
                 <a className='nav__el' href='/login' > Login </a>
                 <a className='nav__el nav__el--cta'> About </a>
             </nav>
